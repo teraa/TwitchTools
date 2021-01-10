d9f1ac38eb032960c4ba0a97b749e1d2e1f5f347
Tera <24725862+teraa@users.noreply.github.com>
2021-01-10 04:59:38 +0100

    update scripts

--- install/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ..
-dotnet publish -c Release -r linux-x64 -o /opt/twitchtools --self-contained false /p:PublishSingleFile=true /p:DebugType=None /p:UseSharedCompilation=false /nodeReuse:false
